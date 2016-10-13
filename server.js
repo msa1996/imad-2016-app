58bd2ca4ac6a2bf03c8c26aa0b461699678a3b29
msa1996 <msadarsh1996@gmail.com>
2016-10-13 12:55:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,13 +24,13 @@ res.sendFile(path.join(__dirname,'ui','article-one.html'));
 
 app.get('/article-two',function(req,res){
     
-res.send('article two has been requested and will be sent');    
+res.sendFile(path.join(__dirname,'ui','article-two.html'));  
     
 });
 
 app.get('/article-three',function(req,res){
     
-res.send('article three has been requested and will be sent');   
+res.sendFile(path.join(__dirname,'ui','article-three.html'));
     
 });
 
