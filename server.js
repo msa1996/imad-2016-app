cc4d8f9cab7de1398eb4901a160afa94314251c1
msa1996 <msadarsh1996@gmail.com>
2016-10-13 11:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,6 +22,19 @@ res.send('article one has been requested and will be sent');
     
 });
 
+app.get('/article-two',function(req,res){
+    
+res.send('article two has been requested and will be sent');    
+    
+});
+
+app.get('/article-three',function(req,res){
+    
+res.send('article three has been requested and will be sent');   
+    
+});
+
+ 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
